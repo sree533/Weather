3b5ed26df2bde9f98a7639cff7a2166db4177e48
Sreekanth <Sreekanthcit1@gmail.com>
2017-11-01 00:52:41 -0500

    Created desired UI

--- WeatherProject/app/src/main/java/com/app/weatherproject/activity/MainActivity.java
+++ WeatherProject/app/src/main/java/com/app/weatherproject/activity/MainActivity.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 
 import com.app.weatherproject.R;
 
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
     }
 }
